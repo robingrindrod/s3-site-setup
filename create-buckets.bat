37dd5e9a23cad9a01b209742edb4f05362c1ff72
Robin Grindrod <robin.grindrod@ericsson.com>
2014-03-13 12:13:43 +0530

    Added setup of public read permissions, setup web hosting on www. bucket and redircted naked domain bucket to www. bucket.

--- create-buckets.bat
+++ create-buckets.bat
@@ -3,3 +3,7 @@ set domain=%1
 
 call aws s3 mb s3://%domain%
 call aws s3 mb s3://www.%domain%
+
+call aws s3api put-bucket-policy --bucket www.%domain% --policy "{\"Statement\": [{\"Effect\": \"Allow\", \"Principal\": {\"AWS\": \"*\"}, \"Action\": \"s3:GetObject\", \"Resource\": \"arn:aws:s3:::www.%domain%/*\"}]}"
+call aws s3api put-bucket-website --bucket www.%domain% --website-configuration "{\"IndexDocument\": {\"Suffix\": \"index.html\"}}"
+call aws s3api put-bucket-website --bucket %domain% --website-configuration "{\"RedirectAllRequestsTo\": {\"HostName\": \"www.%domain%\"}}"
